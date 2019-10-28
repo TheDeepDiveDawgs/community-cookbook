55351b16b3a3944a5b4510c8d8571bcd961d02d0
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-27 19:18:43 -0600

    added cross section of interaction class

--- php/classes/Interaction.php
+++ php/classes/Interaction.php
@@ -1,1 +1,20 @@
 <?php
+
+	/**
+ 	* cross section of of interaction class
+ 	**/
+
+	class interaction {
+
+		/**foreign key**/
+		private $interactionUserId;
+
+		/**foreign key**/
+		private $interactionRecipeId;
+
+		/**Date of rating**/
+		private $interactionDate;
+
+		/**user rating of recipe**/
+		private $interactionRating;
+	}
