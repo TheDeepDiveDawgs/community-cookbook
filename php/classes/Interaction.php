57bc89b904d722a8876cb1f9c908f1bc6f30c840
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-27 21:07:21 -0600

    added accessor and mutator for interaction user id note as of this time using uuid

--- php/classes/Interaction.php
+++ php/classes/Interaction.php
@@ -17,4 +17,47 @@
 
 		/**user rating of recipe**/
 		private $interactionRating;
+
+		/**accessor method for interactionUserId
+		 *@return Uuid value of interactionUserId
+		 **/
+		public function getInteractionUserId(): Uuid {
+			return ($this->interactionUserId);
+		}
+
+		/**
+		 * mutator method for interactionUserId
+		 * @param uuid | string $newInteractionUserId value of new interaction user id
+		 * @throws \RangeException if the $interactionUserId is not positive
+		 * @throws \TypeError if $interactionUserId is not positive
+		 * */
+
+		public function setInteractionUserId($newInteractionUserId): void {
+			try {
+				$uuid = self::validateUuid($newInteractionUserId);
+			} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception){
+				$exceptionType = get_class($exception);
+				throw(new $exceptionType($exception->getMessage(), 0, $exception));
+			}
+
+		//convert and store interactionUserId
+		$this->interactionUserId = $uuid;
+
+		}
+
+		/**
+		 * accessor method for interactionRecipeId
+		 * @returns string value of interactionRecipeId
+		 */
+
+		public function getInteractionRecipeId():string{
+			return ($this->interactionRecipeId);
+		}
+
+		/**
+		 * mutator method for interaction recipe id
+		 * @param string $newInteractionRecipeId new value of interaction user id
+		 * @throws \InvalidArgumentException if $newInteractionRecipeId is not a string
+		 * @throws \RangeException if $newInteractionRecipeId is > than
+		 */
 	}
