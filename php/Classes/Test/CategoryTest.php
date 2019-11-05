67443d2c163a4b71ef4c72bdcfd604e918f8aedf
Floribella Ponce <fponce2@cnm.edu>
2019-11-05 09:09:54 -0700

    added namespace and required files to php unit test file

---
+++ php/Classes/Test/CategoryTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TheDeepDiveDawgs\CommunityCookbook;
+use TheDeepDiveDawgs\CommunityCookbook\{User, Category, Recipe, Interaction};
+
+//grab the calss under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+//grab the uuid generator
+require_once(dirname(__DIR__) . "/lib/uuid.php");
