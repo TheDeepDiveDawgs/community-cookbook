c725519eb32cb26eb69a4b84c10cb2a7071bfee6
Floribella Ponce <fponce2@cnm.edu>
2019-11-05 10:52:07 -0700

    added doc block for unit test

--- php/Classes/Test/CategoryTest.php
+++ php/Classes/Test/CategoryTest.php
@@ -8,3 +8,19 @@ require_once(dirname(__DIR__) . "/autoload.php");
 
 //grab the uuid generator
 require_once(dirname(__DIR__) . "/lib/uuid.php");
+
+/**
+ * Full PHPUnit test for the Category class
+ *
+ * This is a complete PHPUnit test of the Category class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see \TheDeepDiveDawgs\CommunityCookbook\Category
+ * @author Floribella Ponce <fponce2@cnm.edu>
+ **/
+
+class CategoryTest extends CommunityCookbookTest {
+	/**
+	 *valid category name
+	 */
+}
