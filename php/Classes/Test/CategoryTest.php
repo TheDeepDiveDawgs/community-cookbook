9111a65a506fc8bc7871f5bf81d13c62b1cb1c66
Floribella Ponce <fponce2@cnm.edu>
2019-11-05 16:29:15 -0700

    created insert valid function

--- php/Classes/Test/CategoryTest.php
+++ php/Classes/Test/CategoryTest.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace TheDeepDiveDawgs\CommunityCookbook;
+
 use TheDeepDiveDawgs\CommunityCookbook\{User, Category, Recipe, Interaction};
 
-//grab the calss under scrutiny
+//grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
 
 //grab the uuid generator
@@ -18,9 +19,30 @@ require_once(dirname(__DIR__) . "/lib/uuid.php");
  * @see \TheDeepDiveDawgs\CommunityCookbook\Category
  * @author Floribella Ponce <fponce2@cnm.edu>
  **/
-
 class CategoryTest extends CommunityCookbookTest {
 	/**
-	 *valid category name
+	 *valid category name to create the category
+	 * @var $VALID_CATEGORY_NAME
 	 */
+	protected $VALID_CATEGORY_NAME;
+
+	/**
+	 *test inserting a valid category and verifying that the actual mySQL data matches
+	 */
+	public function testInsertValidCategory(): void {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("category");
+
+		//create a new category and insert into mySQL
+		$categoryId = generateUuidV4();
+		$category = new Category($categoryId, $this->VALID_CATEGORY_NAME);
+		$category->insert($this->getPDO());
+
+		//grab the data from mySQL and enforce the fields match our expectations
+		$pdoCategory = Category::getCategoryByCategoryId($this->getPDO(), $category->getCategoryId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("category"));
+		$this->assertEquals($pdoCategory->getCategoryId(), $categoryId);
+		$this->assertEquals($pdoCategory->getCategoryName(), $this->VALID_CATEGORY_NAME);
+	}
+
 }
