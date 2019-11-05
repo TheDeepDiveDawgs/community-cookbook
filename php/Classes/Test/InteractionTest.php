c63c849a3a003fa9e69666c33a57d86e44ac5dc4
Daniel Hernandez <dhernandez43@cnm.edu>
2019-11-05 13:43:16 -0700

    added both foreign keys of user and recipe id for interaction test protected

--- php/Classes/Test/InteractionTest.php
+++ php/Classes/Test/InteractionTest.php
@@ -1,5 +1,6 @@
 <?php
 namespace TheDeepDiveDawgs\CommunityCookbook\Test;
+use TheDeepDiveDawgs\CommunityCookbook\CommunityCookbookTest;
 use TheDeepDiveDawgs\CommunityCookBook\{
 		User, Category, Recipe, Interaction
 };
@@ -8,4 +9,33 @@ use TheDeepDiveDawgs\CommunityCookBook\{
 require_once(dirname(__DIR__) . "autoload.php");
 
 //grab the uuid generator
-require_once(dirname(__DIR__, 1) . "lib/uuid.php");
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+/*
+ * Full PHP unit test for the interaction class
+ *
+ * This is a complete unit test for the interaction class, complete due to all mySQL/PDO
+ * enable methods are tested valid and invalid inputs
+ *
+ * @see interaction
+ *
+ * @author Daniel Hernandez
+ */
+
+class InteractionTest extends CommunityCookbookTest {
+
+	/**
+	 * User that created the Interaction, this is for foreign key relations
+	 * @var User $user
+	 **/
+
+	protected $user = null;
+
+	/**
+	 * Recipe that the Interaction was based on, this is for foreign key relations
+	 * @var Recipe $user
+	 */
+
+	protected $recipe = null;
+
+}
