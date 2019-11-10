b913a780225d39962057971afb1cb838a9e3bd9f
Daniel Hernandez <dhernandez43@cnm.edu>
2019-11-09 20:17:45 -0700

    added protected interaction, validemail validfullname validhandle

--- php/Classes/Test/InteractionTest.php
+++ php/Classes/Test/InteractionTest.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace TheDeepDiveDawgs\CommunityCookbook\Test;
 
-use TheDeepDiveDawgs\CommunityCookbook\{
-	User, Category, Recipe, Interaction
-};
+use TheDeepDiveDawgs\CommunityCookbook\{User, Recipe, Interaction};
 
 //grab class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
@@ -13,70 +10,89 @@ require_once(dirname(__DIR__) . "/autoload.php");
 require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 /*
- * Full PHP unit test for the interaction class
+ * Full PHP unit test for the Interaction class
  *
- * This is a complete unit test for the interaction class, complete due to all mySQL/PDO
+ * This is a complete unit test for the Interaction class, complete due to all mySQL/PDO
  * enable methods are tested valid and invalid inputs
  *
- * @see interaction
- *
+ * @see Interaction
  * @author Daniel Hernandez
  */
 
 class InteractionTest extends CommunityCookbookTest {
 
 	/**
-	 * User that created the Interaction, this is for foreign key relations
-	 * @var User $interactionUserId
+	 * User that created the interaction, this is for foreign key relations
+	 * @var User $user
 	 **/
-
 	protected $user = null;
+
+	/**
+	 * Recipe that the interaction was based on, this is for foreign key relations
+	 * @var Recipe $user
+	 */
+	protected $recipe = null;
+
+	/**
+	 * Interaction of a recipe by a user, this is for foreign key relations
+	 * @var Interaction $interaction
+	 */
+	protected $interaction = null;
 
 	/**
 	 * valid user hash to create the user object to own the test
 	 * @var $VALID_HASH
 	 */
-
-	protected $VALID_USER_HASH;
+	protected $VALID_HASH;
 
 	/**
-	 * Recipe that the Interaction was based on, this is for foreign key relations
-	 * @var Recipe $user
+	 * valid activationToken to create profile object to own the test
+	 * @var string $VALID_ACTIVATION
 	 */
+	protected $VALID_ACTIVATION;
 
-	protected $recipe = null;
+	/**
+	 * valid email to use
+	 * @var string $VALID_EMAIL
+	 */
+	protected $VALID_EMAIL = "grievxus@outlook.com";
+	/**
+	 * valid full name for user
+	 * @var string $VALID_FULLNAME
+	 **/
+	protected $VALID_FULLNAME = "Gino Villalpando";
+	/**
+	 * valid handle to use
+	 * @var string $VALID_HANDLE
+	 **/
+	protected $VALID_HANDLE = "@grievous";
 
 	/**
 	 *timestamp of interaction; this starts at null and is assigned later
 	 * @var \DateTime $VALID_INTERACTIONDATE
 	 */
-
 	protected $VALID_INTERACTIONDATE = null;
 
 	/**
 	 * valid timestamp to use as sunriseInteractionDate
 	 */
-
 	protected $VALID_SUNRISEDATE = null;
 
 	/**
 	 * valid timestamp to use as sunsetInteractionDate
 	 */
-
 	protected $VALID_SUNSETDATE = null;
 
 	/**
 	 * rating of interaction
 	 * @var int $VALID_INTERACTIONRATING
 	 */
-
 	protected $VALID_INTERACTIONRATING = "PHPUnit Test Passing";
 
 	/**
 	 * content of updated rating
 	 * @var int $VALID_INTERACTIONRATING2
 	 */
-
 	protected $VALID_INTERACTIONRATING2 = "PHPUnit Test Still Passing";
 
 	/**
@@ -87,10 +103,11 @@ class InteractionTest extends CommunityCookbookTest {
 		//run default setUp() method first
 		parent::setUp();
 		$password = "abc123";
-		$this->VALID_USER_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
+		$this->VALID_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
+		$this->VALID_ACTIVATION = bin2hex(random_bytes(16));
 
 		//create and insert User to own the test Interaction
-		$this->user = new User(generateUuidV4(), $this->VALID_ACTIVATION, $this->VALID_EMAIL, $this->VALID_FULLNAME, $this->VALID_HANDLE, $this->VALID_USER_HASH);
+		$this->user = new User(generateUuidV4(), $this->VALID_ACTIVATION, $this->VALID_EMAIL, $this->VALID_FULLNAME, $this->VALID_HANDLE, $this->VALID_HASH);
 
 		//calculate the date (use the the time the unit test was setup)
 		$this->VALID_INTERACTIONDATE = new \DateTime();
