a0ae14d3fe85d333017fe8df9a2a0b5ba672db6b
Floribella Ponce <fponce2@cnm.edu>
2019-11-18 11:04:33 -0700

    started rest api for category; added require once and use links

---
+++ php/public_html/apis/category/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/lib/uuid.php";
+
+use TheDeepDiveDawgs\CommunityCookbook\Category;
