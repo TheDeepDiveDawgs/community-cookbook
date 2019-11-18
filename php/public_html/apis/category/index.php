5a3e9ed645744e73c1205b4cc7a7f872b89e015f
Floribella Ponce <fponce2@cnm.edu>
2019-11-18 11:37:49 -0700

    created setup for class

--- php/public_html/apis/category/index.php
+++ php/public_html/apis/category/index.php
@@ -8,3 +8,38 @@ require_once dirname(__DIR__, 3) . "/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 
 use TheDeepDiveDawgs\CommunityCookbook\Category;
+
+/**
+ * API for the Category class
+ *
+ * @author Floribella Ponce <fponce2@cnm.edu>
+ * @version 0.0.1
+ */
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the MySQL connection
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/Secrets.php");
+	$pdo = $secrets->getPdoObject();
+
+	//determine which HTTP method was used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$categoryName = filter_input(INPUT_GET, "categoryName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	//make sure the id is valid for methods that require it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+		throw (new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+}
