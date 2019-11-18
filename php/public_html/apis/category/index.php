2f14634a3d318a4ec641a548dc29fccab06d7702
Floribella Ponce <fponce2@cnm.edu>
2019-11-18 15:18:40 -0700

    removed put, post, and delete method. Cleaned rest of code.

--- php/public_html/apis/category/index.php
+++ php/public_html/apis/category/index.php
@@ -34,9 +34,8 @@ try {
 	//determine which HTTP method was used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
-	//sanitize input
+	//sanitize category input
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$categoryName = filter_input(INPUT_GET, "categoryName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	//make sure the id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
@@ -45,69 +44,21 @@ try {
 
 	//GET method for Category class
 	if($method === "GET") {
+
 		//set XSRF cookie
 		setXsrfCookie();
 
-		// get a specific category bases on arguments or all the categories and update name
+		// get a specific category based on arguments or all the categories
 		if(empty($id) === false) {
+			// get category by Id
 			$reply->data = Category::getCategoryByCategoryId($pdo, $id);
 		} else {
+			// get all categories if no category id is specified
 			$reply->data = Category::getAllCategories($pdo)->toArray();
 		}
-
-		// PUT and POST method for Category
-	} else if($method === "PUT" || $method === "POST") {
-
-		//enforce the user has a XSRF cookie
-		verifyXsrf();
-
-		//Retrieve the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-		$requestContent = file_get_contents("php://input");
-
-		//Decode the JSON package and store the result in $requestObject
-		$requestObject = json_decode($requestContent);
-
-		//Retrieve te category to be updated
-		$category = Category::getCategoryByCategoryId($pdo, $id);
-		if($category === null) {
-			throw(new \RuntimeException("Category does not exist", 404));
-		}
-
-		//category name
-		if(empty($requestObject->categoryName) === true) {
-			throw(new \InvalidArgumentException(("No category name present", 405));
-		}
-
-		$category->setCategoryName($requestObject->categoryName);
-		$category->update($pdo);
-
-		//update category
-		$reply->message = "Category name updated";
-
-	} elseif($method === "DELETE") {
-
-		//verify the XSRF cookie
-		verifyXsrf();
-
-		//retrieve the category to be deleted
-		$category = Category::getCategoryByCategoryId($pdo, $id);
-		if($category === null) {
-			throw(new RuntimeException("Category does not exist", 404));
-		}
-
-		//perform the actual delete
-		$category->delete($pdo);
-
-		//update the message
-		$reply->message = "Category was successfully deleted";
 	}
-
-	// if any other HTTP request is sent throw an exception
-} else {
-	throw new \InvalidArgumentException("invalid http request", 400);
-}
-//catch any exceptions that is thrown and update the reply status and message
-} catch (\Exception | \TypeError $exception) {
+} catch
+(\Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 }
@@ -117,7 +68,7 @@ if($reply->data === null) {
 	unset($reply->data);
 }
 
-//encode and return reply to front end called
+//encode and return reply to front end caller
 echo json_encode($reply);
 
 
