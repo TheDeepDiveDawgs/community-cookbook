4679df23c92899a28a398ff2fe0f12738d257385
Daniel Hernandez <dhernandez43@cnm.edu>
2019-11-19 16:55:26 -0700

    removed 2nd secrets, redundant

--- php/public_html/apis/interaction/index.php
+++ php/public_html/apis/interaction/index.php
@@ -5,7 +5,7 @@ require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
-require_once("/etc/apache2/capstone-mysql/Secrets.php");
+
 use TheDeepDiveDawgs\CommunityCookbook\Interaction;
 
 /**
