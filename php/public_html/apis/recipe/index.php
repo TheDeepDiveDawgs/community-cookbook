59a04a4856de96c4710bdc5026066a24fceaa689
Damian Arya <dahakcyrus@yahoo.com>
2019-11-21 08:49:59 -0700

    replaced key words

--- php/public_html/apis/recipe/index.php
+++ php/public_html/apis/recipe/index.php
@@ -8,12 +8,12 @@ require_once dirname(__DIR__, 3) . "/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
-use UssHopper\DataDesign\{Like, Profile, Tweet};
+use UssHopper\CommunityCookbook\{Search, User, Recipe};
 
 /**
- * api for the Tweet class
+ * api for the Recipe class
  *
- * @author Valente Meza <valebmeza@gmail.com>
+ * @author damian Arya <darya@cnm.edu>
  **/
 
 //verify the session, start if not active
@@ -27,7 +27,7 @@ $reply->status = 200;
 $reply->data = null;
 try {
 
-	$secrets = new \Secrets("/etc/apache2/capstone-mysql/ddctwitter.ini");
+	$secrets = new \Secrets("/etc/apache2/CommunityCookbook-mysql/ddcRecipe.ini");
 	$pdo = $secrets->getPdoObject();
 
 	//determine which HTTP method was used
@@ -35,45 +35,45 @@ try {
 
 	//sanitize input
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
-	$tweetProfileId = filter_input(INPUT_GET, "tweetProfileId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
-	$tweetContent = filter_input(INPUT_GET, "tweetContent", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$recipeUserId = filter_input(INPUT_GET, "recipeUserId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$recipeSearchTerm = filter_input(INPUT_GET, "recipeSearchTerm", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	//make sure the id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true )) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 402));
 	}
 
-	// handle GET request - if id is present, that tweet is returned, otherwise all tweets are returned
+	// handle GET request - if id is present, that recipe is returned, otherwise all recipes are returned
 	if($method === "GET") {
 
 		//set XSRF cookie
 		setXsrfCookie();
 
-		//get a specific tweet or all tweets and update reply
+		//get a specific recipe or all recipes and update reply
 		if(empty($id) === false) {
-			$reply->data = Tweet::getTweetByTweetId($pdo, $id);
-		} else if(empty($tweetProfileId) === false) {
-			// if the user is logged in grab all the tweets by that user based  on who is logged in
-			$reply->data = Tweet::getTweetByTweetProfileId($pdo, $tweetProfileId);
+			$reply->data = Recipe::getRecipeByRecipeId($pdo, $id);
+		} else if(empty($recipeUserId) === false) {
+			// if the user is logged in grab all the recipes by that user based  on who is logged in
+			$reply->data = Recipe::getRecipeByRecipeUserId($pdo, $recipeUserId);
 
-		} else if(empty($tweetContent) === false) {
-			$reply->data = Tweet::getTweetByTweetContent($pdo, $tweetContent)->toArray();
+		} else if(empty($recipeSearchTerm) === false) {
+			$reply->data = Recipe::getRecipeByRecipeSearchTerm($pdo, $recipeSearchTerm)->toArray();
 
 		} else {
-			$tweets = Tweet::getAllTweets($pdo)->toArray();
-			$tweetProfiles = [];
-			foreach($tweets as $tweet){
-				$profile = 	Profile::getProfileByProfileId($pdo, $tweet->getTweetProfileId());
-				$tweetProfiles[] = (object)[
-					"tweetId"=>$tweet->getTweetId(),
-					"tweetProfileId"=>$tweet->getTweetProfileId(),
-					"tweetContent"=>$tweet->getTweetContent(),
-					"tweetDate"=>$tweet->getTweetDate()->format("U.u") * 1000,
-					"profileAtHandle"=>$profile->getProfileAtHandle(),
-					"profileAvatarUrl"=>$profile->getProfileAvatarUrl(),
+			$recipes = Recipe::getAllRecipes($pdo)->toArray();
+			$recipeUsers = [];
+			foreach($recipes as $recipe){
+				$user = 	User::getUserByUserId($pdo, $recipe->getRecipeUserId());
+				$recipeUsers[] = (object)[
+					"recipeId"=>$recipe->getRecipeId(),
+					"recipeUserId"=>$recipe->getRecipeUserId(),
+					"recipeSearchTerm"=>$recipe->getRecipeSearchTerm(),
+					"recipeDate"=>$recipe->getRecipeDate()->format("U.u") * 1000,
+					"userAtHandle"=>$user->getUserAtHandle(),
+					"userAvatarUrl"=>$user->getUserAvatarUrl(),
 				];
 			}
-			$reply->data = $tweetProfiles;
+			$reply->data = $recipeUsers;
 		}
 	} else if($method === "PUT" || $method === "POST") {
 
@@ -81,70 +81,70 @@ try {
 		verifyXsrf();
 
 		// enforce the user is signed in
-		if(empty($_SESSION["profile"]) === true) {
-			throw(new \InvalidArgumentException("you must be logged in to post tweets", 401));
+		if(empty($_SESSION["user"]) === true) {
+			throw(new \InvalidArgumentException("you must be logged in to post recipes", 401));
 		}
 
-		$requestContent = file_get_contents("php://input");
+		$requestSearchTerm = file_get_searchTerms("php://input");
 
-		// Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-		$requestObject = json_decode($requestContent);
+		// Retrieves the JSON package that the front end sent, and stores it in $requestSearchTerm. Here we are using file_get_searchTerms("php://input") to get the request from the front end. file_get_searchTerms() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
+		$requestObject = json_decode($requestSearchTerm);
 
 		// This Line Then decodes the JSON package and stores that result in $requestObject
 
-		//make sure tweet content is available (required field)
-		if(empty($requestObject->tweetContent) === true) {
-			throw(new \InvalidArgumentException ("No content for Tweet.", 405));
+		//make sure recipe searchTerm is available (required field)
+		if(empty($requestObject->recipeSearchTerm) === true) {
+			throw(new \InvalidArgumentException ("No searchTerm for Recipe.", 405));
 		}
 
-		// make sure tweet date is accurate (optional field)
-		if(empty($requestObject->tweetDate) === true) {
-			$requestObject->tweetDate = null;
+		// make sure recipe date is accurate (optional field)
+		if(empty($requestObject->recipeDate) === true) {
+			$requestObject->recipeDate = null;
 		}
 
 		//perform the actual put or post
 		if($method === "PUT") {
 
-			// retrieve the tweet to update
-			$tweet = Tweet::getTweetByTweetId($pdo, $id);
-			if($tweet === null) {
-				throw(new RuntimeException("Tweet does not exist", 404));
+			// retrieve the recipe to update
+			$recipe = Recipe::getRecipeByRecipeId($pdo, $id);
+			if($recipe === null) {
+				throw(new RuntimeException("Recipe does not exist", 404));
 			}
 
 			//enforce the end user has a JWT token
 
-			//enforce the user is signed in and only trying to edit their own tweet
-			if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $tweet->getTweetProfileId()->toString()) {
-				throw(new \InvalidArgumentException("You are not allowed to edit this tweet", 403));
+			//enforce the user is signed in and only trying to edit their own recipe
+			if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $recipe->getRecipeUserId()->toString()) {
+				throw(new \InvalidArgumentException("You are not allowed to edit this recipe", 403));
 			}
 
 			validateJwtHeader();
 
 			// update all attributes
 
-			//$tweet->setTweetDate($requestObject->tweetDate);
-			$tweet->setTweetContent($requestObject->tweetContent);
-			$tweet->update($pdo);
+			//$recipe->setRecipeDate($requestObject->recipeDate);
+			$recipe->setRecipeSearchTerm($requestObject->recipeSearchTerm);
+			$recipe->update($pdo);
 
 			// update reply
-			$reply->message = "Tweet updated OK";
+			$reply->message = "Recipe updated OK";
 
 		} else if($method === "POST") {
 
 			// enforce the user is signed in
-			if(empty($_SESSION["profile"]) === true) {
-				throw(new \InvalidArgumentException("you must be logged in to post tweets", 403));
+			if(empty($_SESSION["user"]) === true) {
+				throw(new \InvalidArgumentException("you must be logged in to post recipes", 403));
 			}
 
 			//enforce the end user has a JWT token
 			validateJwtHeader();
 
-			// create new tweet and insert into the database
-			$tweet = new Tweet(generateUuidV4(), $_SESSION["profile"]->getProfileId(), $requestObject->tweetContent, null);
-			$tweet->insert($pdo);
+			// create new recipe and insert into the database
+			$recipe = new Recipe(generateUuidV4(), $_SESSION["user"]->getUserId(), $requestObject->recipeSearchTerm, null);
+			$recipe->insert($pdo);
 
 			// update reply
-			$reply->message = "Tweet created OK";
+			$reply->message = "Recipe created OK";
 		}
 
 	} else if($method === "DELETE") {
@@ -152,24 +152,24 @@ try {
 		//enforce that the end user has a XSRF token.
 		verifyXsrf();
 
-		// retrieve the Tweet to be deleted
-		$tweet = Tweet::getTweetByTweetId($pdo, $id);
-		if($tweet === null) {
-			throw(new RuntimeException("Tweet does not exist", 404));
+		// retrieve the Recipe to be deleted
+		$recipe = Recipe::getRecipeByRecipeId($pdo, $id);
+		if($recipe === null) {
+			throw(new RuntimeException("Recipe does not exist", 404));
 		}
 
-		//enforce the user is signed in and only trying to edit their own tweet
-		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $tweet->getTweetProfileId()->toString()) {
-			throw(new \InvalidArgumentException("You are not allowed to delete this tweet", 403));
+		//enforce the user is signed in and only trying to edit their own recipe
+		if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $recipe->getRecipeUserId()->toString()) {
+			throw(new \InvalidArgumentException("You are not allowed to delete this recipe", 403));
 		}
 
 		//enforce the end user has a JWT token
 		validateJwtHeader();
 
-		// delete tweet
-		$tweet->delete($pdo);
+		// delete recipe
+		$recipe->delete($pdo);
 		// update reply
-		$reply->message = "Tweet deleted OK";
+		$reply->message = "Recipe deleted OK";
 	} else {
 		throw (new InvalidArgumentException("Invalid HTTP method request", 418));
 	}
@@ -181,5 +181,5 @@ try {
 }
 
 // encode and return reply to front end caller
-header("Content-type: application/json");
+header("SearchTerm-type: application/json");
 echo json_encode($reply);
