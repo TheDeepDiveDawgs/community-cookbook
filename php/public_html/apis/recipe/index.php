a432ecc2504705bb2ebd8cc166844d80f8796cb4
Damian Arya <dahakcyrus@yahoo.com>
2019-11-25 17:00:10 -0700

    fixed linesfor stlye compliance

--- php/public_html/apis/recipe/index.php
+++ php/public_html/apis/recipe/index.php
@@ -12,6 +12,7 @@ use TheDeepDiveDawgs\CommunityCookbook\{Category, User, Recipe, Interaction};
  *
  * @author damian Arya <darya@cnm.edu>
  **/
+
 //verify the session, start if not active
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
@@ -28,6 +29,7 @@ try {
 
 	//determine which HTTP method was used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
 	//sanitize input
 	$recipeId = filter_input(INPUT_GET, "recipeId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
 	$recipeUserId = filter_input(INPUT_GET, "recipeUserId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
@@ -44,15 +46,19 @@ try {
 	if(($method === "DELETE" || $method === "PUT") && (empty($recipeId) === true )) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 402));
 	}
+
 	// handle GET request - if id is present, that recipe is returned, otherwise all recipes are returned
 	if($method === "GET") {
+
 		//set XSRF cookie
 		setXsrfCookie();
+
 		//get a specific recipe or all recipes and update reply
 		if(empty($recipeId) === false) {
 			$reply->data = Recipe::getRecipeByRecipeId($pdo, $recipeId);
 		} else if(empty($recipeUserId) === false) {
-			// if the user is logged in grab all the recipes by that user based  on who is logged in
+
+			// if the user is logged in grab all the recipes by that user based on who is logged in
 			$reply->data = Recipe::getRecipeByRecipeUserId($pdo, $recipeUserId);
 		} else if(empty($recipeIngredients | $recipeName | $recipeStep) === false) {
 			$reply->data = Recipe::getRecipeByRecipeSearchTerm($pdo, $recipeIngredients, $recipeName, $recipeStep)->toArray();
@@ -80,37 +86,46 @@ try {
 			$reply->data = $recipeUsers;
 		}
 	} else if($method === "PUT" || $method === "POST") {
+
 		// enforce the user has a XSRF token
 		verifyXsrf();
+
 		// enforce the user is signed in
 		if(empty($_SESSION["user"]) === true) {
 			throw(new \InvalidArgumentException("you must be logged in to post recipes", 401));
 		}
 		$requestContents = file_get_contents("php://input");
+
 		// Retrieves the JSON package that the front end sent, and stores it in $requestSearchTerm. Here we are using file_get_searchTerms("php://input") to get the request from the front end. file_get_searchTerms() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
 		$requestObject = json_decode($requestContents);
+
 		// This Line Then decodes the JSON package and stores that result in $requestObject
 		//make sure recipe searchTerm is available (required field)
 		if(empty($requestObject->recipeDescription) === true) {
 			throw(new \InvalidArgumentException ("No content for Recipe.", 405));
 		}
+
 		// make sure recipe date is accurate (optional field)
 		if(empty($requestObject->recipeSubmissionDate) === true) {
 			$requestObject->recipeSubmissionDate = null;
 		}
+
 		//perform the actual put or post
 		if($method === "PUT") {
+
 			// retrieve the recipe to update
 			$recipe = Recipe::getRecipeByRecipeId($pdo, $recipeId);
 			if($recipe === null) {
 				throw(new RuntimeException("Recipe does not exist", 404));
 			}
+
 			//enforce the end user has a JWT token
 			//enforce the user is signed in and only trying to edit their own recipe
 			if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $recipe->getRecipeUserId()->toString()) {
 				throw(new \InvalidArgumentException("You are not allowed to edit this recipe", 403));
 			}
 			validateJwtHeader();
+
 			// update all attributes
 			//$recipe->setRecipeSubmissionDate($requestObject->recipeSubmissionDate);
 			$recipe->setRecipeDescription($requestObject->recipeDescription);
@@ -118,46 +133,58 @@ try {
 			// update reply
 			$reply->message = "Recipe updated OK";
 		} else if($method === "POST") {
+
 			// enforce the user is signed in
 			if(empty($_SESSION["user"]) === true) {
 				throw(new \InvalidArgumentException("you must be logged in to post recipes", 403));
 			}
+
 			//enforce the end user has a JWT token
 			validateJwtHeader();
+
 			// create new recipe and insert into the database
 			$recipe = new Recipe(generateUuidV4(), $requestObject->recipeCategoryId, $_SESSION["user"]->getUserId(), $requestObject->recipeDescription,
 				$requestObject->recipeImageUrl, $requestObject->recipeIngredients, $requestObject->recipeMinutes, $requestObject->recipeName,
 				$requestObject->recipeNumberIngredients, $requestObject->recipeNutrition, $requestObject->recipeStep, null);
 			$recipe->insert($pdo);
+
 			// update reply
 			$reply->message = "Recipe created OK";
 		}
 	} else if($method === "DELETE") {
+
 		//enforce that the end user has a XSRF token.
 		verifyXsrf();
+
 		// retrieve the Recipe to be deleted
 		$recipe = Recipe::getRecipeByRecipeId($pdo, $recipeId);
 		if($recipe === null) {
 			throw(new RuntimeException("Recipe does not exist", 404));
 		}
+
 		//enforce the user is signed in and only trying to edit their own recipe
 		if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $recipe->getRecipeUserId()->toString()) {
 			throw(new \InvalidArgumentException("You are not allowed to delete this recipe", 403));
 		}
+
 		//enforce the end user has a JWT token
 		validateJwtHeader();
+
 		// delete recipe
 		$recipe->delete($pdo);
+
 		// update reply
 		$reply->message = "Recipe deleted OK";
 	} else {
 		throw (new InvalidArgumentException("Invalid HTTP method request", 418));
 	}
+
 // update the $reply->status $reply->message
 } catch(\Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 }
+
 // encode and return reply to front end caller
 header("Content-type: application/json");
 echo json_encode($reply);
