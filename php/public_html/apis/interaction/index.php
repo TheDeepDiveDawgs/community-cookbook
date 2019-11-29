c6e471437e8f0c811d8a884e44ff0ae176f9ed03
Daniel Hernandez <dhernandez43@cnm.edu>
2019-11-28 21:09:28 -0700

    cleaned up code layout

--- php/public_html/apis/interaction/index.php
+++ php/public_html/apis/interaction/index.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
@@ -39,7 +40,6 @@ try {
 	$interactionRecipeId = $id = filter_input(INPUT_GET, "interactionRecipeId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$interactionRating = filter_input(INPUT_GET, "interactionRating", FILTER_VALIDATE_INT);
 
-
 	if($method === "GET") {
 		//set xsrf cookie
 		setXsrfCookie();
@@ -52,14 +52,16 @@ try {
 			if($interaction !== null) {
 				$reply->data = $interaction;
 			}
+
 			//if none of the search parameters are met throw exception
 		} else if(empty($interactionUserId) === false) {
 			$reply->data = Interaction::getInteractionByInteractionUserId($pdo, $interactionUserId)->toArray();
+
 			//get all the interactions associated with recipeId
 		} else if(empty($interactionRecipeId) === false) {
 			$reply->data = Interaction::getInteractionByInteractionRecipeId($pdo, $interactionRecipeId)->toArray();
 		} else {
-			throw new InvalidArgumentException("incorrect search parameters", 404);
+			throw new InvalidArgumentException("Incorrect Search Parameters", 404);
 		}
 
 	} else if($method === "POST" || $method === "PUT") {
@@ -81,11 +83,11 @@ try {
 			//enforce the end user JWT token
 			//validate JWT header
 			validateJwtHeader();
+
 			//enforce the user signed in
 			if(empty($_SESSION["user"]) === true) {
 				throw(new \InvalidArgumentException("you must be logged in to interact", 403));
 			}
-
 
 			$interaction = new Interaction($_SESSION["user"]->getUserId(), $requestObject->interactionRecipeId,
 				null, $requestObject->interactionRating);
@@ -95,9 +97,6 @@ try {
 		} else if($method === "PUT") {
 			//enforce the end user  has a xsrf token
 			verifyXsrf();
-
-			//enforce the end user has a jwt token
-//			validateJwtHeader();
 
 			//grab the interaction by its composite key
 			// gets recipe to update
@@ -120,15 +119,19 @@ try {
 				$requestObject->interactionRating = $interaction->getInteractionRating();
 			}
 			$interaction->setInteractionRating($requestObject->interactionRating);
+
 			//perform the actual update
 			$interaction->update($pdo);
+
 			//update the message
-			$reply->message = "Interaction successfully updated";
+			$reply->message = "Rating Successfully Updated";
 		}
+
 		//if any other HTTP request is sent throw an exception
 	} else {
 		throw new \InvalidArgumentException("invalid http request, 400");
 	}
+
 	// catch any exceptions that is thrown and update the reply status message
 } catch(\Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
