6214892927be3b471dc5e801915280522b98234d
Gino Villalpando <grieveous@live.com>
2019-11-30 11:51:17 -0700

    adding interaction apis to branch

--- php/public_html/apis/interaction/index.php
+++ php/public_html/apis/interaction/index.php
@@ -6,7 +6,8 @@ require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 
-use TheDeepDiveDawgs\CommunityCookbook\Interaction;
+use TheDeepDiveDawgs\CommunityCookbook\{Interaction, User, Recipe, Category};
+
 
 /**
  * API for updating Interaction
@@ -35,11 +36,12 @@ try {
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	//sanitize search parameters
-	$interactionUserId = $id = filter_input(INPUT_GET, "interactionUserId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$interactionRecipeId = $id = filter_input(INPUT_GET, "interactionRecipeId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$interactionUserId = $userId = filter_input(INPUT_GET, "interactionUserId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$interactionRecipeId = $recipeId = filter_input(INPUT_GET, "interactionRecipeId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$interactionRating = filter_input(INPUT_GET, "interactionRating", FILTER_VALIDATE_INT);
 
 	if($method === "GET") {
-	//set xsrf cookie
+		//set xsrf cookie
 		setXsrfCookie();
 
 		//gets  a specific interaction associated based on its composite key
@@ -49,81 +51,95 @@ try {
 			if($interaction!== null) {
 				$reply->data = $interaction;
 			}
-		//if none of the search parameters are met throw exception
+			//if none of the search parameters are met throw exception
 		} else if(empty($interactionUserId) === false) {
-				$reply->data = Interaction::getInteractionByInteractionUserId($pdo, $interactionUserId)->toArray();
-		//get all the interactions associated with recipeId
+			$reply->data = Interaction::getInteractionByInteractionUserId($pdo, $interactionUserId)->toArray();
+			//get all the interactions associated with recipeId
 		} else if(empty($interactionRecipeId) === false) {
 			$reply->data = Interaction::getInteractionByInteractionRecipeId($pdo, $interactionRecipeId)->toArray();
 		} else {
-				throw new InvalidArgumentException("incorrect search parameters", 404);
+			throw new InvalidArgumentException("incorrect search parameters", 404);
 		}
+
 	} else if($method === "POST" || $method === "PUT") {
 		//decode the response from the front end
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
 		if(empty($requestObject->interactionUserId) === true) {
-				throw (new \InvalidArgumentException("No User linked to Interaction", 405));
+			throw (new \InvalidArgumentException("No User linked to Interaction", 405));
 		}
 		if(empty($requestObject->interactionRecipeId) === true) {
 			throw (new \InvalidArgumentException("No Recipe linked to Interaction", 405));
 		}
+
+
 		if($method === "POST") {
 			//enforce that the end user has a XSRF token.
 			verifyXsrf();
 
 			//enforce the end user JWT token
 			//validate JWT header
+			validateJwtHeader();
 
 			//enforce the user signed in
 			if(empty($_SESSION["user"]) === true) {
-					throw(new \InvalidArgumentException("you must be logged in to interact", 403));
+				throw(new \InvalidArgumentException("you must be logged in to interact", 403));
 			}
-			validateJwtHeader();
 
-			$interaction = new Interaction($_SESSION["user"]->getUserId(), $requestObject->interactionRecipeId);
+
+			$interaction = new Interaction($_SESSION["user"]->getUserId(), $requestObject->interactionRecipeId, null, $requestObject->interactionRating);
+
 			$interaction->insert($pdo);
-			$reply->message = "interaction recipe successful";
+			$reply->message = "interaction rating successful";
 
 		} else if($method === "PUT") {
 			//enforce the end user  has a xsrf token
 			verifyXsrf();
 
+
+
+			//grab the interaction by its composite key
+			// retrieve the recipe to update
+			$interaction = Interaction::getInteractionByInteractionRecipeIdAndInteractionUserId($pdo, $interactionRecipeId, $interactionUserId);
+			if($interaction === null) {
+				throw(new RuntimeException("Interaction does not exist", 404));
+			}
+
+			//enforce the user is signed in and only trying to edit their own interaction
+			if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $interaction->getInteractionUserId()->toString()) {
+				throw(new \InvalidArgumentException("You are not allowed to update this interaction.", 403));
+			}
+
 			//enforce the end user has a jwt token
 			validateJwtHeader();
 
-			//grab the interaction by its composite key
-			$interaction = Interaction::getInteractionByInteractionRecipeIdAndInteractionUserId($pdo, $requestObject->interactionUserId, $requestObject->interactionRecipeId);
-			if($interaction === null) {
-				throw (new RuntimeException("interaction does not exist"));
+			if(empty($requestObject->interactionRating) === true) {
+				$requestObject->interactionRating = $interaction->getInteractionRating();
 			}
-			//enforce the user is signed in and only trying to edit their own interaction
-			if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserIdI()->toString() !== $interaction->getInteractionUserId()->toString()) {
-				throw(new \InvalidArgumentException("You are not allowed to delete this interaction. 403"));
-			}
-			//validate the jwtHeader();
 
-			//perform the actual delete
-			$interaction->delete($pdo);
+			$interaction->setInteractionRating($requestObject->interactionRating);
+
+			//perform the actual update
+			$interaction->update($pdo);
 
 			//update the message
-			$reply->message = "Interaction successfully deleted";
+			$reply->message = "Interaction successfully updated";
 		}
-			//if any other HTTP request is sent throw an exception
+		//if any other HTTP request is sent throw an exception
 	}   else {
-					throw new \InvalidArgumentException("invalid http request, 400");
+		throw new \InvalidArgumentException("invalid http request, 400");
 	}
 	// catch any exceptions that is thrown and update the reply status message
 } catch(\Exception | \TypeError $exception) {
-			$reply->status = $exception->getCode();
-			$reply->message =$exception->getMessage();
+	$reply->status = $exception->getCode();
+	$reply->message =$exception->getMessage();
 }
 
 header("Content-type: application/json");
-	if($reply->data === null) {
-				unset($reply->data);
-	}
+if($reply->data === null) {
+	unset($reply->data);
+}
 
-	//encode and return reply to front end caller
+//encode and return reply to front end caller
 echo json_encode($reply);
