a81aa57718af181a1e2e7acbf4085e5547ceb1e3
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-02 11:53:57 -0700

    started boiler plate to footer js

---
+++ app/src/shared/components/footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {Link} from "react-router-dom";
+
+import '../../../index.css';
+
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import row from "react-bootstrap/Row";
+
+import{FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+export const Footer = () => {
+	<>
+		<footer className={"page-footer"}>
+}
+
