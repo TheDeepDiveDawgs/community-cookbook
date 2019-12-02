1385ceb10d74fe1a352ecb0acc5753a57663d818
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-02 10:41:15 -0700

    deleted footer directory and file, due to creating a new branch

--- app/src/index.js
+++ app/src/index.js
@@ -5,12 +5,14 @@ import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
+import {Footer} from "./pages/footer/Footer";
 
 const Routing = () => (
 	<>
 		<BrowserRouter>
 			<Switch>
 				<Route exact path="/" component={Home}/>
+				<Route exact path="/footer" component={Footer}/>
 				<Route component={FourOhFour}/>
 			</Switch>
 		</BrowserRouter>
