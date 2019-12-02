5408a5ee7daf0a7750131bc43237ab9b46355017
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-02 16:34:25 -0700

    verfied code does compile used test

--- app/src/shared/components/footer/footer.js
+++ app/src/shared/components/footer/footer.js
@@ -1,26 +1,27 @@
 import React from "react";
 
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 
-import '../../../index.css';
+// import '../../../index.css';
 
-import col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import row from "react-bootstrap/Row";
+ import Col from "react-bootstrap/Col";
+ import Container from "react-bootstrap/Container";
+ import Row from "react-bootstrap/Row";
 
-import{FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {Container} from "react-bootstrap";
+
+// import{FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Footer = () => (
 	<>
 		<footer className="page-footer">
-			<Container="true">
-				<row>
-					<col className="text-center">
+			<Container fluid="true">
+				<Row>
+					<Col className="text-center">
 						<p> test </p>
-					</col>
-				</row>
+					</Col>
+				</Row>
 			</Container>
 		</footer>
-	<>
+	</>
 );
+
