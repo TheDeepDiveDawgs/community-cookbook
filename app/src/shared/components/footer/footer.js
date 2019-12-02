36487baed7b397c51d5af13c0ca18fd7fc669d6e
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-02 14:26:31 -0700

    added test code updated footer code

--- app/src/shared/components/footer/footer.js
+++ app/src/shared/components/footer/footer.js
@@ -4,23 +4,23 @@ import {Link} from "react-router-dom";
 
 import '../../../index.css';
 
-import Col from "react-bootstrap/Col";
+import col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import row from "react-bootstrap/Row";
 
 import{FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Container} from "react-bootstrap";
 
 export const Footer = () => (
 	<>
-		<footer className={"page-footer"}>
-			<Container-fluid="true">
+		<footer className="page-footer">
+			<Container="true">
 				<row>
-					<col className={"text-center"}>
+					<col className="text-center">
 						<p> test </p>
 					</col>
 				</row>
-			</Container-fluid>
+			</Container>
 		</footer>
 	<>
 );
-
