db11a36f4f7128d742071c6a0cf7354a9fb46262
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-03 15:22:44 -0700

    add links to social media for cookbook

--- app/src/shared/components/footer/footer.js
+++ app/src/shared/components/footer/footer.js
@@ -9,32 +9,32 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import logo from "./images/logo.png";
 import logo2 from "./images/abqLogo.png";
+import fbLogo from "./images/fbLogoTransparent.jpg";
 
-// import{FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import{FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Footer = () => (
 	<>
-		<footer className="page-footer bg-dark variant=dark fixed-bottom">
+		<footer className="page-footer bg-black fixed-bottom">
 			<Container className="container-fluid">
 				<Row>
 					<Col>
-						<a href="https://facebook.com/">
-						<h6>Like Us on Facebook</h6>
+						<a href="https://https://www.facebook.com/cook.book.3705157">
+						<h5>Like Us on Facebook</h5>
 						</a>
 					</Col>
 					<Col></Col>
 					<Col>
-						<a href="https://twitter.com/?lang=en">
-							<h6> Follow Us on Twitter </h6>
+						<a href="https://twitter.com/CookBoo43086652">
+							<h5> Follow Us on Twitter </h5>
 						</a>
 					</Col>
 					<Col></Col>
 					<Col>
 						<a href="mailto:abqcookbook@gmail.com" target="_blank">
-							<h6> Contact Us </h6>
+							<h5> Contact Us </h5>
 						</a>
 					</Col>
-					<Col></Col>
 				</Row>
 			</Container>
 		</footer>
