866a920cfe597b1005697da451ee5869d15c853a
Damian Arya <dahakcyrus@yahoo.com>
2019-12-03 16:00:23 -0700

    still not sur ewhat to do here

---
+++ app/src/pages/recipe-page.js
@@ -0,0 +1,14 @@
+<Figure>
+	<Figure.Image
+		// i need to put recipe image from database here?
+		width={1000}
+		height={1000}
+		alt="171x180"
+		src="file/image"
+	/>
+	<Figure.Caption>
+		I need to put the recipe info form database here
+	</Figure.Caption>
+</Figure>
+// add in recipe rating
+// also need to add css to style side by side
