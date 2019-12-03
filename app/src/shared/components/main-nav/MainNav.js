fa41646fc615f5d95dc70ba68828a448aa744521
Damian Arya <dahakcyrus@yahoo.com>
2019-12-03 08:46:46 -0700

    toatle revamp of header with new icon

--- app/src/shared/components/main-nav/MainNav.js
+++ app/src/shared/components/main-nav/MainNav.js
@@ -3,32 +3,33 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {LinkContainer} from "react-router-bootstrap"
 // import {SignUpModal} from "./sign-up/SignUpModal";
-//import {SignInModal} from "./sign-in/SigninModal";
-import logo from "./images/logo.png";
+//import {SignInModal} from "./sign-in/SignInModal";
+import logo from "./images/nav-icon.png";
+import css from "./nav-style.css"
 
 export const MainNav = (props) => {
 	return(
-		<Navbar bg="dark" variant="dark">
+		<Navbar className="nav-style">
 			<Navbar.Brand href="/">
-				<img className="cap-logo-4.png"
+				<img className="nav-icon.png"
 					  src= {logo}
-					  width="80"
+					  width="520"
 					  height="80"
 					  className="d-inline-block align-top"
 				/>
 			</Navbar.Brand>
 
 			<LinkContainer exact to="/" >
-				<Navbar.Brand>ABQ Cookbook</Navbar.Brand>
+				<Navbar.Brand></Navbar.Brand>
 			</LinkContainer>
 			<Nav className="mr-auto">
 				<LinkContainer exact to="/profile">
-					<Nav.Link>search</Nav.Link>
+					<Nav.Link></Nav.Link>
 				</LinkContainer>
 				{/*<SignUpModal/>*/}
 				{/*<SignInModal/>*/}
 				<LinkContainer exact to="/image"
-				><Nav.Link>image</Nav.Link>
+				><Nav.Link></Nav.Link>
 				</LinkContainer>
 			</Nav>
 		</Navbar>
