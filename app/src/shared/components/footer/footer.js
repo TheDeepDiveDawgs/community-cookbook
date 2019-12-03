642453e150438e1d9b3952d5c6ee4ecb754e7dbb
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-02 22:37:14 -0700

    working on adding logos to footer

--- app/src/shared/components/footer/footer.js
+++ app/src/shared/components/footer/footer.js
@@ -2,23 +2,38 @@ import React from "react";
 
 // import {Link} from "react-router-dom";
 
-// import '../../../index.css';
+import '../../../index.css';
 
- import Col from "react-bootstrap/Col";
- import Container from "react-bootstrap/Container";
- import Row from "react-bootstrap/Row";
-
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import logo from "./images/logo.png";
+import logo2 from "./images/abqLogo.png";
 
 // import{FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Footer = () => (
 	<>
-		<footer className="page-footer">
-			<Container fluid="true">
+		<footer className="page-footer bg-dark variant=dark container-fluid">
+			<Container>
 				<Row>
-					<Col className="text-center">
-						<p> test </p>
+					<ul>
+					<Col class="col-1">
+						<img className="logo.png" alt="logo"
+						src={logo}
+						width="70"
+						height="70"
+						/>
 					</Col>
+					<Col class="col-10">
+					</Col>
+					<Col class="col-1">
+						<img className="logo.png" alt="logo"
+							  src={logo}
+							  width="70"
+							  height="70"/>
+					</Col>
+					</ul>
 				</Row>
 			</Container>
 		</footer>
