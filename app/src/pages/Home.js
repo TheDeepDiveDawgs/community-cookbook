1b29ae3afec70d3f2b5db6af9e6596f56314c990
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-04 16:26:03 -0700

    added boiler plate bootstrap imports'

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,4 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
+
+import {useSelector, useDispatch} from "react-redux";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/es/FormControl";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import CardGroup from "react-bootstrap/CardGroup";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar"
+import CardDeck from "react-bootstrap/CardDeck";
 
 export const Home = () => {
 	return (
