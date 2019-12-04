165fa69a1803cd35200722f9344b23d59984860b
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-04 09:29:47 -0700

    note ready for burn, all links functional and working as expected at this time

--- app/src/shared/components/footer/footer.js
+++ app/src/shared/components/footer/footer.js
@@ -1,14 +1,11 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import ReactDom from 'react-dom';
+// import {Link} from "react-router-dom";
+// import ReactDom from 'react-dom';
 import '../../../index.css';
 
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import logo from "./images/logo.png";
-import logo2 from "./images/abqLogo.png";
-import fbLogo from "./images/fbLogoTransparent.jpg";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
@@ -28,7 +25,7 @@ export const Footer = () => (
 								Like Us on Facebook
 							</h5>
 							<i className="d-lg-none float-left">
-								<FontAwesomeIcon icon={faFacebookF} size="2x" color="yellow" />
+								<FontAwesomeIcon icon={faFacebookF} size="2x"  />
 							</i>
 						</a>
 					</Col>
@@ -39,18 +36,18 @@ export const Footer = () => (
 								Follow Us on Twitter
 							</h5>
 							<i className="d-lg-none d-flex justify-content-center">
-								<FontAwesomeIcon icon={faTwitter} size="2x" color="yellow" />
+								<FontAwesomeIcon icon={faTwitter} size="2x" />
 							</i>
 						</a>
 					</Col>
 
 					<Col>
-						<a href="mailto:abqcookbook@gmail.com" target="_blank">
+						<a href="mailto:abqcookbook@gmail.com" rel="noopener noreferrer" target="_blank">
 							<h5 className="d-none d-lg-block float-right">
 								Contact Us
 							</h5>
 							<i className="d-lg-none float-right">
-								<FontAwesomeIcon icon={faEnvelope} size="2x" color="yellow" />
+								<FontAwesomeIcon icon={faEnvelope} size="2x" />
 							</i>
 						</a>
 					</Col>
