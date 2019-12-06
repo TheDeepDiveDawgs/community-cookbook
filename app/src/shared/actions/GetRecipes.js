8bd5005ea71aa5a0cd42404c40663c0a5b27db70
Floribella Ponce <fponce2@cnm.edu>
2019-12-06 00:41:23 -0700

    added recipe actions

---
+++ app/src/shared/actions/GetRecipes.js
@@ -0,0 +1,22 @@
+import {httpConfig} from "../utils/http-config";
+
+export const getRecipeByRecipeId = (id) => async (dispatch) => {
+	const {data} = await httpConfig(`/apis/recipe/${id}`);
+	dispatch({type: "GET_RECIPE_BY_RECIPE_ID", payload: data})
+};
+
+export const getRecipeByRecipeUserId = (userId) => async (dispatch) => {
+	const {data} = await httpConfig(`/apis/recipe/?recipeUserId=${userId}`);
+	dispatch({type: "GET_RECIPE_BY_RECIPE_USER_ID", payload: data})
+};
+
+export const getRecipeBySearchTerm = () => async (dispatch) => {
+	const {data} = await httpConfig(`/apis/recipe/`);
+	dispatch({type: "GET_RECIPE_BY_SEARCH_TERM", payload: data})
+};
+
+export const getAllRecipe = () => async (dispatch) => {
+	const {data} = await httpConfig(`/apis/recipe/`);
+	dispatch({type: "GET_ALL_RECIPE", payload: data})
+};
+
