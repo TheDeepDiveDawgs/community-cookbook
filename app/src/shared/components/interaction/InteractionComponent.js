b66837fc2c1e4ae6adef8cab8b7cf362b26eb742
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-05 17:49:25 -0700

    started initial interaction style code

--- app/src/shared/components/interaction/InteractionComponent.js
+++ app/src/shared/components/interaction/InteractionComponent.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from "react-redux";
-import {getAllInteractions} from "../../actions/interaction";
+import {getAllInteractions} from "../../actions/interactionAction";
 import Card from "react-bootstrap/Card";
 
 export const Home = () => {
