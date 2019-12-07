5a06813c7a9d350465dbf38d84f6a9712e0974e6
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-07 14:39:30 -0700

    git added the interaction comp so get a visual of the rating  on home page, note may be remvoed once recipe is set up

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {InteractionComp} from "../shared/components/interaction/InteractionComponent";
 
 export const Home = () => {
 	return (
 		<>
 			<h1>Home</h1>
+			<InteractionComp/>
 		</>
 	)
 };
