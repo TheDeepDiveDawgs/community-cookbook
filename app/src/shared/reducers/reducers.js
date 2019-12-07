2c7c31018609f7424a4af97700981981a48e56ee
Damian Arya <dahakcyrus@yahoo.com>
2019-12-07 14:39:37 -0700

    created reducer

--- app/src/shared/reducers/reducers.js
+++ app/src/shared/reducers/reducers.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import UserReducer from "./UserReducer";
 import interactionReducer from "./interactionReducer";
+import recipeReducer from "./RecipeReducer";
 
 export default combineReducers({
 	user:UserReducer,
 	interactions: interactionReducer,
+	recipe: recipeReducer,
 })
