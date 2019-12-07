64cf59a781a042c1a7246e420221f41b50ce41e2
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-07 14:42:59 -0700

    commented our boiler plater code working on getting interaction from ids

---
+++ app/src/shared/actions/get-recipe-interactions.js
@@ -0,0 +1,6 @@
+// import {httpConfig} from "../../../src/shared/utils/http-config";
+//
+// export const getRecipeInteractions = (recipeId) => async dispatch => {
+// 		const {data} = await httpConfig('/apis/recipe/')
+
+// }
