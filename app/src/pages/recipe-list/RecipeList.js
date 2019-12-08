c5991509ba3174f2c679cd957fb5ff941bfe0b68
Floribella Ponce <fponce2@cnm.edu>
2019-12-07 21:19:26 -0700

    created recipe list componenet

---
+++ app/src/pages/recipe-list/RecipeList.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {CategoryCard} from "./CategoryCard";
+import {SearchBar} from "./SearchBar";
+import {Recipes} from "./Recipes";
+
+export const RecipeList = () => {
+	return (
+		<main>
+			<div>
+				<SearchBar/>
+			</div>
+			<div className="container">
+				<div className="row">
+					<div className="col-3">
+						<CategoryCard/>
+					</div>
+					<div className="col-9">
+						<Recipes/>
+					</div>
+				</div>
+			</div>
+		</main>
+	)
+};
