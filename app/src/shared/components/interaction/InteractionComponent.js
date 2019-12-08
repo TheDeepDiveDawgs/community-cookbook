98e1c68fd36b4c413e10b8c52f91322ab66f6b4b
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-08 14:31:30 -0700

    added const inputs for reciep id and user id and ratgins to amke sure it is being updated from redux for component rendering

--- app/src/shared/components/interaction/InteractionComponent.js
+++ app/src/shared/components/interaction/InteractionComponent.js
@@ -8,7 +8,7 @@ import {handleSessionTimeout} from "../../../shared/misc/handle-session-timeout"
 import _ from "lodash";
 import Card from "react-bootstrap/Card";
 
-export const Rating = ({userId, RecipeId}) => {
+export const Rating = ({userId, recipeId}) => {
 	//grab the JWT token for logged in users
 	const jwt =UseJwt();
 
@@ -17,13 +17,14 @@ export const Rating = ({userId, RecipeId}) => {
 	"active" is a bootstrap class  that will be added later to the star rating
 	 */
 	const [isRated, setIsRated] = useState(null);
+	const [ratingCount, setRatingCount] = useState(0);
 
 	//return all ratings from the redux store
 	const ratings = useSelector(state => (state.ratings ? state.ratings : []));
 
 	const effects = () => {
 		initializeRatings(userId);
-
+		countRatings(recipeId);
 	};
 
 	/* add ratings to inputs - informs react that ratings are being updated from Redux
@@ -31,8 +32,6 @@ export const Rating = ({userId, RecipeId}) => {
 	 */
 	const inputs = [ratings, userId, recipeId];
 	useEffect(effects, inputs);
-
-
 };
 
 
