868119c21d65045850a09b130b001bfdcb59ce2b
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-09 16:43:32 -0700

    udpated to display avg on screen

--- app/src/shared/components/interaction/InteractionComponent.js
+++ app/src/shared/components/interaction/InteractionComponent.js
@@ -1,34 +1,56 @@
 import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from "react-redux";
-import {httpConfig} from "../../utils/http-config";
-import {UseJwtUserId} from "../../utils/JwtHelpers";
-import {UseJwt} from "../../utils/JwtHelpers";
-import {UseJwtUserHandle} from "../../utils/JwtHelpers";
-import {handleSessionTimeout} from "../../../shared/misc/handle-session-timeout"
-import _ from "lodash";
 import Card from "react-bootstrap/Card";
-import {getRecipeByRecipeId} from "../../actions/recipeActions";
+import {getRecipeInteractions} from "../../actions/interactionAction";
+import Ratings from 'react-ratings-declarative';
 
+export const InteractionComponent = (props) => {
+	const {recipeId} = props;
+	//returns the user(s) from redux and assigns it to the users variable
+	const interactions = useSelector(state => state.interactions ? state.interactions : []);
+	//assigns useDispatch reference to the dispatch variable for late use
+	const dispatch = useDispatch();
+	//define the side effects that will occur in app
+	//E.G. Code that handles dispatches to redux , api requests, or times
+	function sideEffects() {
+		//dispatch function takes actions as arguments to make changes to the store/redux
+		dispatch(getRecipeInteractions(recipeId))
+	}
+	const sideEffectsInputs = [recipeId];
 
+	useEffect(sideEffects, sideEffectsInputs);
 
-import {getAllInteractions, getRecipeInteraction, getUserInteraction} from "../../actions/recipeActions";
+	console.log(interactions.length);
+
+	const reducer = (accumulator, interaction) => accumulator.interactionRating + interaction.interactionRating;
+	 // console.log(interactions.reduce(reducer));
+
+	const average = (interactions.length ? interactions.reduce(reducer)/interactions.length : "No Interactions yet");
 
 	return (
-		<>
 
-			{interactions.map(interaction => {
-				return (
-					<>
-					<Card style={{width: '18rem'}}>
-					<Card.Img/>
-					<Card.Body>
-						<Card.Text> Rating </Card.Text>
-					</Card.Body>
-					</Card>
-					</>
-				)
 
-			})}
-		</>
+						<Card style={{width: '18rem'}} >
+							<Card.Img/>
+							<Card.Body>
+								<Card.Text> Rating {average} </Card.Text>
+							</Card.Body>
+						</Card>
+
+
+
+
 	)
 };
+
+
+
+
+
+
+
+
+
+
+
+
