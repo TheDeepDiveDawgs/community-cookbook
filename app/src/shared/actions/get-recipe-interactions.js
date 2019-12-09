e8acd2ef05da62a755f15861f2270210da86f9de
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-09 10:20:20 -0700

    added and moved all actions to the interactionAction

--- app/src/shared/actions/get-recipe-interactions.js
+++
@@ -1,6 +0,0 @@
-// import {httpConfig} from "../../../src/shared/utils/http-config";
-//
-// export const getRecipeInteractions = (recipeId) => async dispatch => {
-// 		const {data} = await httpConfig('/apis/recipe/')
-
-// }
--- app/src/shared/actions/interactionAction.js
+++ app/src/shared/actions/interactionAction.js
@@ -1,6 +1,22 @@
-// import {httpConfig} from "../utils/http-config";
+import {httpConfig} from "../../../src/shared/utils/http-config";
 
-// export const getAllInteractions = () => async (dispatch) => {
-// 		const payload = await httpConfig.get("/apis/interaction/");
-// 		dispatch({type: "GET_ALL_INTERACTIONS", payload : payload.data});
-// };
+export const getAllInteractions = () => async (dispatch) => {
+ 		const payload = await httpConfig.get("/apis/interaction/");
+ 		dispatch({type: "GET_ALL_INTERACTIONS", payload : payload.data});
+ };
+
+export const getRecipeInteraction = (recipeId) => async dispatch => {
+	const {data} = await httpConfig('/apis/recipe/?interactionRecipeId=${recipeId}');
+	dispatch({type: "GET_RECIPE_INTERACTIONS", payload: data})
+};
+
+export const getUserInteraction = (userId) => async dispatch => {
+	const {data} = await httpConfig('/apis/recipe/?interactionUserId=${userId}');
+	dispatch({type: "GET_USER_INTERACTIONS", payload: data})
+};
+
+export const getInteractionByInteractionRecipeIdAndInteractionUserId = (recipeIdAndUserId) => async dispatch => {
+	const {data} = await httpConfig('/apis/recipe/?interactionUserId=${userId}');
+	dispatch({type: "GET_SPECIFIC_INTERACTION", payload: data})
+
+};
