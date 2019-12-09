32b41ada77aa58da51bcdc653afeb26ac5f1505c
Floribella Ponce <fponce2@cnm.edu>
2019-12-09 14:13:18 -0700

    added variables

--- app/src/pages/recipe-list/RecipeCard.js
+++ app/src/pages/recipe-list/RecipeCard.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import Card from "react-bootstrap/Card";
 
-export const RecipeCard = () => {
-		return (
-			<Card class="container">
-				<Card.Body className="row">
-					<div className="col-3">
+export const RecipeCard = ({recipe}) => {
+	return (
+		<Card class="container">
+			<Card.Body className="row">
+				<div className="col-3">
 					<Card.Img src="cap-logo-4.png" alt="placeholder"/>
-					</div>
-					<div className="col-9">
-					<Card.Title> Recipe </Card.Title>
-					<Card.Subtitle>Rating, Cooktime, Ingredients </Card.Subtitle>
-					<Card.Text>Description of recipe</Card.Text>
-					</div>
-				</Card.Body>
-			</Card>
-		);
+				</div>
+				<div className="col-9">
+					<Card.Title> {recipe.recipeName}</Card.Title>
+					<Card.Subtitle>{recipe.recipeMinutes}</Card.Subtitle>
+					<Card.Text>{recipe.recipeDescription}</Card.Text>
+				</div>
+			</Card.Body>
+		</Card>
+	);
 };
