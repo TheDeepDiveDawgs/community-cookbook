03a5efcefd86193485c908f91f3c110a4f3f3f8c
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-09 16:45:56 -0700

    added interaction comp to view while working on rating

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {InteractionComponent} from "../../shared/components/interaction/InteractionComponent";
 
 export const Home = () => {
 	return (
 		<>
 			<h1>Home</h1>
+			<InteractionComponent recipeId="00392c66-dd53-4a7a-85c7-975e100b777a"/>
 		</>
 	)
 };
