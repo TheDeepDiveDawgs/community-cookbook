92e88f632f1aaf28897de4dffb24d021115fd102
Damian Arya <dahakcyrus@yahoo.com>
2019-12-10 09:09:55 -0700

    fixed style bg issue

--- app/src/pages/recipe-page/RecipePage.js
+++ app/src/pages/recipe-page/RecipePage.js
@@ -1,8 +1,8 @@
 import React, {useEffect} from 'react';
 import './recipe-page-css.css';
-import {Route} from 'react-router';
 import {getRecipeByRecipeId} from "../../shared/actions/recipeActions";
 import {useDispatch, useSelector} from "react-redux";
+import logo from "./cap-logo-5.png"
 
 export const RecipePage = (props) => {
 	const {match} = props;
@@ -16,7 +16,7 @@ export const RecipePage = (props) => {
 
 	return (
 		<section className="margin">
-			<div className="container-fluid bg-secondary py-5">
+			<div className="container-fluid py-5">
 				<div className="row">
 					<div className="col-md-9">
 							<h3 id="recipeName">Recipe</h3>
@@ -25,7 +25,7 @@ export const RecipePage = (props) => {
 							<p id="rating">{recipe.recipeRating}</p>
 							{/*<h4 id="recipeInteraction">interaction</h4>*/}
 							{/*<p id="interaction">{recipe.recipeInteraction}</p>*/}
-						{/*need to add danny's rating above*/}
+				{/*need to add danny's rating above*/}
 							<h4 id="recipeDescription">Description</h4>
 							<p id="description">{recipe.recipeDescription}</p>
 							<h4 id="recipeNumberIngredients">how many ingredients</h4>
@@ -36,7 +36,7 @@ export const RecipePage = (props) => {
 							<p id="steps">{recipe.recipeStep}</p>
 							<h4 id="recipeNutrition">Nutrition</h4>
 							<p id="nutrition">{recipe.recipeNutrition}</p>
-							<h4 id="recipeMinutes">minutes to make</h4>
+							<h4 id="recipeMinutes">cook time</h4>
 							<p id="minutes">{recipe.recipeMinutes}</p>
 							<h4 id="recipeSubmissionDate">recipe submitted</h4>
 							<p id="submissionDate">{recipe.recipeSubmissionDate}</p>
@@ -44,72 +44,13 @@ export const RecipePage = (props) => {
 					<div className="col-md-3">
 						<h3 id="recipeImageUrl">image</h3>
 						<p id="imageUrl">{recipe.recipeImageUrl}</p>
-						<img className="card-img-top" id="cardImg" src={recipe.recipeImageUrl ? recipe.recipeImageUrl : "./recipe-page/cap-logo-5.png"} alt="recipe image"/>
+						<img className="card-img-top" id="cardImg" src={recipe.recipeImageUrl ? recipe.recipeImageUrl : ({logo})} alt="recipe image"/>
 					</div>
 				</div>
 			</div>
 		</section>
 	)
 };
-
-//
-//   return (
-//
-{/*<section>*/}
-{/*	// <div className="container-fluid bg-secondary py-5">*/}
-{/*	// <div className="row">*/}
-{/*	// <div className="col-md-10">*/}
-{/*	// <div className="">*/}
-{/*	// <div className="recipe-card-size">*/}
-{/*	// <div className="recipe-title">*/}
-{/*	// <h1>recipe title</h1>*/}
-{/*	// </div>*/}
-{/*	// <br></br>*/}
-{/*	// <div>*/}
-{/*	// <strong>this is where rating will be placed</strong>*/}
-{/*	// </div>*/}
-{/*	// <br></br>*/}
-{/*	// <div>*/}
-{/*	// <p>concise recipe description gets populated here</p>*/}
-{/*	// </div>*/}
-{/*	// <div>*/}
-{/*	// <strong>Ingredients</strong>*/}
-{/*	// <ul>*/}
-{/*	// <li>ingredient one</li>*/}
-{/*	// <li>ingredient two</li>*/}
-{/*	// <li>ingredient three</li>*/}
-{/*	// </ul>*/}
-{/*	// </div>*/}
-{/*	// <div className="recipe-card-content">*/}
-{/*	// <div>*/}
-{/*	// <ul>*/}
-{/*	// <strong>Steps</strong>*/}
-{/*	// <li>step one</li>*/}
-{/*	// <li>step two</li>*/}
-{/*	// <li>step three</li>*/}
-{/*	// </ul>*/}
-{/*	// </div>*/}
-{/*	// <div>*/}
-{/*	// <strong>nutrition</strong>*/}
-{/*	// <p>this is where the nutritional information will be populated</p>*/}
-{/*	// </div>*/}
-{/*	// <div>min to make recipe</div>*/}
-{/*	// <br></br>*/}
-{/*	// <div>recipe submission date will be auto populated here</div>*/}
-{/*	// </div>*/}
-{/*	// </div>*/}
-{/*	// </div>*/}
-{/*	// </div>*/}
-{/*	// <div className="col-md-2">*/}
-{/*	// <div>*/}
-{/*	// <img src="cap-logo-5.png" alt="logo"/>*/}
-{/*	// </div>*/}
-{/*	// </div>*/}
-{/*	// </div>*/}
-{/*	// </div>*/}
-{/*	// </section>*/}
-{/*//   )*/}
-{/*// };*/}
 
 {/*<h1 id="title"><em>{recipe.recipeName}</em></h1>*/}
 {/*<img className="card-img-top" id="cardImg"*/}
