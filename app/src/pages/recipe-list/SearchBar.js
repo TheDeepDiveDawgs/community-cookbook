71b20efbe1b5ca7947346f063041b0f8edecfd7c
Floribella Ponce <fponce2@cnm.edu>
2019-12-10 09:35:46 -0700

    removed container and adjusted columns for row

--- app/src/pages/recipe-list/SearchBar.js
+++ app/src/pages/recipe-list/SearchBar.js
@@ -1,17 +1,19 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import Form from "react-bootstrap/Form";
+import './recipe-list-styles.css';
 
 export const SearchBar = () => {
 	return (
-		<Form.Group controlId="formBasicEmail" className="container">
+		<Form.Group>
 			<div className="row">
-				<div className="col-9">
-					<Form.Control type="text" placeholder="Search by recipe name (ex. enchiladas), ingredient (ex. chile, or instructions (ex. oven)."/>
+				<div className="offset-md-1 col-10">
+					<Form.Control type="text" name="search-keywords" placeholder="Search by recipe name (ex. enchiladas), ingredient (ex. chile, or instructions (ex. oven)."/>
 				</div>
-				<div className="col-3">
+				<div className="col-1">
 					<button type="button">Search</button>
 				</div>
 			</div>
 		</Form.Group>
 	);
 };
+
