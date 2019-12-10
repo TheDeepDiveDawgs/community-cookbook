62b534909dd8abd3f5d1407cedd4911b845b6950
Gino Villalpando <grievxus@outlook.com>
2019-12-09 17:09:21 -0700

    trying to get search box to display data

--- app/src/pages/recipe-list/CategoryCard.js
+++ app/src/pages/recipe-list/CategoryCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 
--- app/src/shared/components/main-nav/search/SearchForm.js
+++ app/src/shared/components/main-nav/search/SearchForm.js
@@ -1,30 +1,32 @@
 import React from "react";
 import Button from 'react-bootstrap/Button'
 import Form from "react-bootstrap/Form";
+// import {getRecipeBySearchTerm} from "../../../actions/recipeActions";
 import FormControl from "react-bootstrap/FormControl";
 import {httpConfig} from "../../../utils/http-config";
 
 
 export const SearchFormContent = () => {
 
-	const searchTerm = () => {
+	const searchTerm = (e) => {
 		httpConfig.get('apis/recipe/')
-			.then(reply => {
+			.then( reply => {
 				if (reply.status === 200) {
-					console.log(reply);
-					window.location = "/recipe-list";
+					console.log(e);
 				}
 			})
 	};
 
+
+
 	return (
 		<>
 			<Form inline className="ml-auto" id="search-box">
-				<FormControl type="text" placeholder="Search for recipe" id="search-text"/>
+				<input type="text" placeholder="Search for recipe" id="search-text" onChange={searchTerm}/>
 				<Button className="btn btn-dark mx-4 px-4 py-2 text-white"
 						variant="outline-dark"
 						id="search-button"
-						type="submit"
+						type="reset"
 						onSubmit={searchTerm}
 				>
 					Search</Button>
