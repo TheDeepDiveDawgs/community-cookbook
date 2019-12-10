c1284366986a276fa056152c00361bf7f6f5f671
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-10 13:35:05 -0700

    add star system

--- app/src/shared/components/interaction/InteractionComponent.js
+++ app/src/shared/components/interaction/InteractionComponent.js
@@ -2,56 +2,55 @@ import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import Card from "react-bootstrap/Card";
 import {getRecipeInteractions} from "../../actions/interactionAction";
-import Ratings from 'react-ratings-declarative'
-import {Component} from 'react'
+import Ratings from 'react-ratings-declarative';
 
 
-class Foo extends Component {
-	changeRating( newRating ) {
-		this.setState({
-			rating: newRating
-		});
-	}
+// class Foo extends Component {
+// 	changeRating( newRating ) {
+// 		this.setState({
+// 			rating: newRating
+// 		});
+// 	}
 
-	render() {
-		return (
-			<Ratings
-				rating={this.state.rating}
-				widgetRatedColors="blue"
-				changeRating={this.changeRating}
-			>
-				<Ratings.Widget />
-				<Ratings.Widget />
-				<Ratings.Widget
-					widgetDimension="60px"
-					svgIconViewBox="0 0 5 5"
-					svgIconPath="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z"
-				/>
-				<Ratings.Widget widgetHoverColor="black" />
-				<Ratings.Widget />
-			</Ratings>
-		);
-	}
-}
+	// render() {
+	// 	return (
+	// 		<Ratings
+	// 			rating={this.state.rating}
+	// 			widgetRatedColors="blue"
+	// 			changeRating={this.changeRating}
+	// 		>
+	// 			<Ratings.Widget />
+	// 			<Ratings.Widget />
+	// 			<Ratings.Widget
+	// 				widgetDimension="60px"
+	// 				svgIconViewBox="0 0 5 5"
+	// 				svgIconPath="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z"
+	// 			/>
+	// 			<Ratings.Widget widgetHoverColor="black" />
+	// 			<Ratings.Widget />
+	// 		</Ratings>
+	// 	);
+	// }
+// }
 
 
-class Bar extends Component {
-	render() {
-		return (
-			<Ratings
-				rating={3.403}
-				widgetDimensions="40px"
-				widgetSpacings="15px"
-			>
-				<Ratings.Widget widgetRatedColor="green" />
-				<Ratings.Widget widgetSpacing="30px" widgetDimension="15px" />
-				<Ratings.Widget widgetRatedColor="black" />
-				<Ratings.Widget widgetRatedColor="rebeccapurple" />
-				<Ratings.Widget />
-			</Ratings>
-		);
-	}
-}
+// class Bar extends Component {
+// 	render() {
+// 		return (
+// 			<Ratings
+// 				rating={3.403}
+// 				widgetDimensions="40px"
+// 				widgetSpacings="15px"
+// 			>
+// 				<Ratings.Widget widgetRatedColor="green" />
+// 				<Ratings.Widget widgetSpacing="30px" widgetDimension="15px" />
+// 				<Ratings.Widget widgetRatedColor="black" />
+// 				<Ratings.Widget widgetRatedColor="rebeccapurple" />
+// 				<Ratings.Widget />
+// 			</Ratings>
+// 		);
+// 	}
+// }
 
 
 export const InteractionComponent = (props) => {
@@ -77,35 +76,34 @@ export const InteractionComponent = (props) => {
 
 	const average = (interactions.length ? interactions.reduce(reducer)/interactions.length : "0");
 
-	return (
 
+		return (
 
-						<Card style={{width: '18rem'}} >
-							<Card.Img/>
-							<Card.Body>
-								<Ratings
-									rating={average}
-									widgetRatedColors="blue"
-									// changeRating={}
+			<Card style={{width: '12rem',}}>
+				<Card.Body >
+					<Ratings
+									rating={+average}
+									widgetRatedColors="#F5FF38"
+									changeRating={+average}
+									widgetHoverColors="#F5FF38"
+									widgetDimensions="1rem"
 								>
-									<Ratings.Widget />
-									<Ratings.Widget />
-									<Ratings.Widget
-										widgetDimension="60px"
-										svgIconViewBox="0 0 5 5"
-										svgIconPath="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z"
+						 			<Ratings.Widget  />
+						 			<Ratings.Widget  />
+						 			<Ratings.Widget
+
+										// svgIconViewBox="0 0 5 5"
+										// svgIconPath="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z"
 									/>
-									<Ratings.Widget widgetHoverColor="black" />
-									<Ratings.Widget />
-								</Ratings>
-								<Card.Text > Rating {average}</Card.Text>
-							</Card.Body>
-						</Card>
+						 			<Ratings.Widget  />
+						 			<Ratings.Widget  />
+						 		</Ratings>
 
 
+				</Card.Body>
+			</Card>
+		)
 
-
-	)
 };
 
 
