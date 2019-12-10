6cf3f49ba445121f4c742b1209c2676c1e2b06e7
Daniel Hernandez <dhernandez43@cnm.edu>
2019-12-10 08:24:17 -0700

    started working on code to get a star rating to render

--- app/src/shared/components/interaction/InteractionComponent.js
+++ app/src/shared/components/interaction/InteractionComponent.js
@@ -2,7 +2,57 @@ import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import Card from "react-bootstrap/Card";
 import {getRecipeInteractions} from "../../actions/interactionAction";
-import Ratings from 'react-ratings-declarative';
+import Ratings from 'react-ratings-declarative'
+import {Component} from 'react'
+
+
+class Foo extends Component {
+	changeRating( newRating ) {
+		this.setState({
+			rating: newRating
+		});
+	}
+
+	render() {
+		return (
+			<Ratings
+				rating={this.state.rating}
+				widgetRatedColors="blue"
+				changeRating={this.changeRating}
+			>
+				<Ratings.Widget />
+				<Ratings.Widget />
+				<Ratings.Widget
+					widgetDimension="60px"
+					svgIconViewBox="0 0 5 5"
+					svgIconPath="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z"
+				/>
+				<Ratings.Widget widgetHoverColor="black" />
+				<Ratings.Widget />
+			</Ratings>
+		);
+	}
+}
+
+
+class Bar extends Component {
+	render() {
+		return (
+			<Ratings
+				rating={3.403}
+				widgetDimensions="40px"
+				widgetSpacings="15px"
+			>
+				<Ratings.Widget widgetRatedColor="green" />
+				<Ratings.Widget widgetSpacing="30px" widgetDimension="15px" />
+				<Ratings.Widget widgetRatedColor="black" />
+				<Ratings.Widget widgetRatedColor="rebeccapurple" />
+				<Ratings.Widget />
+			</Ratings>
+		);
+	}
+}
+
 
 export const InteractionComponent = (props) => {
 	const {recipeId} = props;
@@ -25,7 +75,7 @@ export const InteractionComponent = (props) => {
 	const reducer = (accumulator, interaction) => accumulator.interactionRating + interaction.interactionRating;
 	 // console.log(interactions.reduce(reducer));
 
-	const average = (interactions.length ? interactions.reduce(reducer)/interactions.length : "No Interactions yet");
+	const average = (interactions.length ? interactions.reduce(reducer)/interactions.length : "0");
 
 	return (
 
@@ -33,7 +83,22 @@ export const InteractionComponent = (props) => {
 						<Card style={{width: '18rem'}} >
 							<Card.Img/>
 							<Card.Body>
-								<Card.Text> Rating {average} </Card.Text>
+								<Ratings
+									rating={average}
+									widgetRatedColors="blue"
+									// changeRating={}
+								>
+									<Ratings.Widget />
+									<Ratings.Widget />
+									<Ratings.Widget
+										widgetDimension="60px"
+										svgIconViewBox="0 0 5 5"
+										svgIconPath="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z"
+									/>
+									<Ratings.Widget widgetHoverColor="black" />
+									<Ratings.Widget />
+								</Ratings>
+								<Card.Text > Rating {average}</Card.Text>
 							</Card.Body>
 						</Card>
 
