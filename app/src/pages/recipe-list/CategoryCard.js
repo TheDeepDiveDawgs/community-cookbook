2225fd475f91d1a921190db6470b0df4bf781aad
Floribella Ponce <fponce2@cnm.edu>
2019-12-10 09:31:35 -0700

    removed href

--- app/src/pages/recipe-list/CategoryCard.js
+++ app/src/pages/recipe-list/CategoryCard.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import Card from "react-bootstrap/Card";
+import React from 'react';
 import ListGroup from "react-bootstrap/ListGroup";
 
 export const CategoryCard = ({category}) => {
 	return (
 		<div>
 			<ListGroup variant="flush">
-				<ListGroup.Item action variant="category" href="#">{category.categoryName}</ListGroup.Item>
+				<ListGroup.Item action variant="category" href="">{category.categoryName}</ListGroup.Item>
 			</ListGroup>
 		</div>
 	);
