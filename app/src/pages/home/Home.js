451953339d9ea8f7df83c1c1a75e19108a59c8eb
Gino Villalpando <gvillalpando@cnm.edu>
2019-12-11 00:42:13 -0700

    added further styling and changed bg on landing portion of home page

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -6,9 +6,9 @@ export const Home = () => {
 	return (
 		<>
 			<Jumbotron fluid className="mt-3 jumbo-Image">
-				<div className="row mt-5">
-					<div className="col-xs-6 col-md-3 text-center p-0">
-						<div className="py-3 px-0 text-bg text-white">
+				<div className="row-fluid mt-5">
+					<div className="col-xs-6 col-md-3 text-center p-0 pl-lg-5 mr-auto">
+						<div className="container py-5 text-bg text-white">
 							<Link to="/recipe-list"
 								  className="display-4 text-white"
 								  id="browseRecipes"
@@ -16,9 +16,9 @@ export const Home = () => {
 						</div>
 					</div>
 				</div>
-				<div className="row-fluid">
+				<div className="row-fluid mt-5">
 					<div className="mb-0 p-0 landingText-position col-xs-6 col-md-3 text-right mr-5 ml-auto">
-						<div className="container py-3 text-bg text-white">
+						<div className="container py-5 text-bg text-white">
 							<ul className="text-white text-landing mb-0"
 							   id="browseRecipes"
 							>
