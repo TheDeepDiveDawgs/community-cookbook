705c346763c19ee1dfb2ab73f98cfdbf3b2657c5
Floribella Ponce <fponce2@cnm.edu>
2019-12-10 23:43:56 -0700

    created event listeners

--- app/src/pages/recipe-list/SearchBar.js
+++ app/src/pages/recipe-list/SearchBar.js
@@ -2,12 +2,18 @@ import React, {useEffect} from 'react';
 import Form from "react-bootstrap/Form";
 import './recipe-list-styles.css';
 
-export const SearchBar = () => {
+export const SearchBar = ({setSearchTerm}) => {
+	//setting a function expression
+	const setSearch = (event) => {
+		console.log("Hello Recipe", event.target.value);
+		setSearchTerm(event.target.value);
+	};
+
 	return (
 		<Form.Group>
 			<div className="row">
 				<div className="offset-md-1 col-9">
-					<Form.Control type="text" name="search-keywords" placeholder="Search by recipe name (ex. enchiladas), ingredient (ex. chile, or instructions (ex. oven)."/>
+					<Form.Control type="text"  onChange={setSearch} name="search-keywords" placeholder="Search by recipe name (ex. enchiladas), ingredient (ex. chile, or instructions (ex. oven)."/>
 				</div>
 				<div className="onset-md-1 col-1">
 					<button type="button">Search</button>
