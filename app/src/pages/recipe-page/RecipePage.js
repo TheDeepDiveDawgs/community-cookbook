cdcf3e3d701ceba213fdc11066417a2b734db0e0
Floribella Ponce <fponce2@cnm.edu>
2019-12-10 17:09:39 -0700

    created map fucntion and declared weHaveData variable

--- app/src/pages/recipe-page/RecipePage.js
+++ app/src/pages/recipe-page/RecipePage.js
@@ -13,49 +13,54 @@ export const RecipePage = (props) => {
 	}, [match.params.recipeId]);
 	const filterRecipe = recipes.filter(recipe => recipe.recipeId === match.params.recipeId);
 	const recipe = {...filterRecipe[0]};
-
+	let weHaveData = (!(Object.entries(recipe).length === 0 && recipe.constructor === Object));
 	return (
 		<section className="margin">
 			<div className="container-fluid py-5">
 				<div className="row">
 					<div className="col-md-9">
-							<h2><strong><p id="name">{recipe.recipeName}</p></strong></h2>
+						<h2><strong><p id="name">{recipe.recipeName}</p></strong></h2>
 						<div className="row">
 							<div className="col-md-4">
-							<h4 id="recipeRating">rating</h4>
-							<p id="rating">{recipe.recipeRating}</p>
-							{/*<h4 id="recipeInteraction">interaction</h4>*/}
-							{/*<p id="interaction">{recipe.recipeInteraction}</p>*/}
-				{/*need to add danny's rating above*/}
+								<h4 id="recipeRating">rating</h4>
+								<p id="rating">{recipe.recipeRating}</p>
+								{/*<h4 id="recipeInteraction">interaction</h4>*/}
+								{/*<p id="interaction">{recipe.recipeInteraction}</p>*/}
+								{/*need to add danny's rating above*/}
 							</div>
 							<div className="col-md-4">
-							<h4 id="recipeNumberIngredients"># of ingredients</h4>
-							<p id="numberIngredients">{recipe.recipeNumberIngredients}</p>
+								<h4 id="recipeNumberIngredients"># of ingredients</h4>
+								<p id="numberIngredients">{recipe.recipeNumberIngredients}</p>
 							</div>
 							<div className="col-md-4">
-							<h4 id="recipeMinutes">cook time <small>(minutes)</small></h4>
-							<p id="minutes">{recipe.recipeMinutes}</p>
+								<h4 id="recipeMinutes">cook time <small>(minutes)</small></h4>
+								<p id="minutes">{recipe.recipeMinutes}</p>
 							</div>
 						</div>
-					<div className="ingred">
+						<div className="ingred">
 							<h4 id="recipeDescription">Description</h4>
 							<p id="description">{recipe.recipeDescription}</p>
-						<div className="ul">
-							<h4 id="recipeIngredients">Ingredients</h4>
-							<p id="ingredients">{recipe.recipeIngredients}</p>
-						</div>
+							<div className="ul">
+								<h4 id="recipeIngredients">Ingredients</h4>
+								<ul>
+									{weHaveData && recipe.recipeIngredients.map(ingredient => <li> {ingredient} </li>)}
+								</ul>
+							</div>
 							<h4 id="recipeSteps">Steps</h4>
-							<p id="steps">{recipe.recipeStep}</p>
+							<ol id="steps">
+								{weHaveData && recipe.recipeStep.map(step => <li> {step} </li>)}
+							</ol>
 							<h4 id="recipeNutrition">Nutrition</h4>
 							<p id="nutrition">{recipe.recipeNutrition}</p>
 							<h5 id="recipeSubmissionDate">recipe submitted</h5>
 							<p id="submissionDate">{recipe.recipeSubmissionDate}</p>
-					</div>
+						</div>
 					</div>
 					<div className="col-md-3">
 						<h3 id="recipeImageUrl">image</h3>
 						<p id="imageUrl">{recipe.recipeImageUrl}</p>
-						<img className="card-img-top" id="cardImg" src={recipe.recipeImageUrl ? recipe.recipeImageUrl : logo} alt="recipe image"/>
+						<img className="card-img-top" id="cardImg" src={recipe.recipeImageUrl ? recipe.recipeImageUrl : logo}
+							  alt="recipe image"/>
 					</div>
 				</div>
 			</div>
@@ -63,6 +68,9 @@ export const RecipePage = (props) => {
 	)
 };
 
-{/*<img className="card-img-top" id="cardImg"*/}
-{/*src={recipe.recipeImageUrl ? recipe.recipeMedia : "./recipe-page/cap-logo-5.png"}*/}
-{/*alt="logo image as default"/>*/}
+{/*<img className="card-img-top" id="cardImg"*/
+}
+{/*src={recipe.recipeImageUrl ? recipe.recipeMedia : "./recipe-page/cap-logo-5.png"}*/
+}
+{/*alt="logo image as default"/>*/
+}
