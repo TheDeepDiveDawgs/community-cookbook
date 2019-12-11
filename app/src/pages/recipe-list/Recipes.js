e78749b700b93f127965c2b7b9d6be1e96c9e564
Floribella Ponce <fponce2@cnm.edu>
2019-12-11 10:41:49 -0700

    commented out recipe steps from filter and added recipe description

--- app/src/pages/recipe-list/Recipes.js
+++ app/src/pages/recipe-list/Recipes.js
@@ -9,7 +9,14 @@ export const Recipes = ({searchTerm}) => {
 	// assigning value of filtered recipes to recipes
 	const recipesState = useSelector(state => (state.recipe ? state.recipe : []));
 
-	const filteredRecipes = recipesState.filter(recipe => recipe.recipeName.includes(searchTerm) || recipe.recipeIngredients.includes(searchTerm) || recipe.recipeStep.includes(searchTerm));
+	//this created the filter
+	// commented out filtering through recipe steps since full step needs to be searched. To prevent affecting UX the recipe steps have been commented out
+	const filteredRecipes = recipesState.filter(recipe => recipe.recipeName.includes(searchTerm) ||  recipe.recipeDescription.includes(searchTerm) || recipe.recipeIngredients.includes(searchTerm)/*|| recipe.recipeStep.includes(searchTerm)*/);
+
+	// check if recipe search term is coming back with a value
+	// console.log("recipe search term = ", searchTerm);
+
+	const recipes = filteredRecipes, dispatch = useDispatch();
 
 	function sideEffects() {
 		dispatch(getAllRecipe());
@@ -22,7 +29,7 @@ export const Recipes = ({searchTerm}) => {
 	return (
 		<>
 				<div>
-					{filteredRecipes.map(recipe => <RecipeCard key={recipe.recipeId} recipe={recipe}/>
+					{recipes.map(recipe => <RecipeCard key={recipe.recipeId} recipe={recipe}/>
 					)}
 				</div>
 		</>
