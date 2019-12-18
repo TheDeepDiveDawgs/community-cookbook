acffc45ba84d207284f7b1ac7f6bd6f64ffc7484
Damian Arya <dahakcyrus@yahoo.com>
2019-12-17 19:42:51 -0700

    finished cleaning up spacing and tabs on submission page

--- app/src/pages/submission-page/SubmissionPage.js
+++ app/src/pages/submission-page/SubmissionPage.js
@@ -56,87 +56,44 @@ class RecipeSubmission extends Component {
 		const {title, instructions, img, ingredients} = this.state;
 		const {onClose} = this.props;
 		let submissions = ingredients.map((ing, i) => (
-			< div
-				className="recipe-form-line"
-				key={`ingredient-${i}`
-				}>
-				<
-					label> {i + 1}.
-					< submission
-						type="text"
-						name={`ingredient-${i}`
-						}
-						value={ing}
-						size={45}
-						autoComplete="off"
-						placeholder=" Ingredient"
-						onChange={this.handleChangeIng}
-					/>
-					< /label>
-						< /div>
-							))
-							;
+
+			<div className="recipe-form-line" key={`ingredient-${i}`}>
+				<label> {i + 1}.
+					<submission type="text" name={`ingredient-${i}`} value={ing} size={45} autoComplete="off" placeholder="Ingredient"
+					onChange={this.handleChangeIng}/>
+				</label>
+			</div>
+	)) ;
 
 							return (
-							< div
-								className="recipe-form-container">
-								< form
-									className='recipe-form'
-									onSubmit={this.handleSubmit}>
-									< button
-										type="button"
-										className="close-button"
-										onClick={onClose}> X
-									</button>
-									< div className='recipe-form-line'>
+		<div className="recipe-form-container">
+			<form className='recipe-form' onSubmit={this.handleSubmit}>
+				<button type="button" className="close-button" onClick={onClose}>X</button>
 
-										< label
-											htmlFor='recipe-title-submission'> Title </label>
+					<div className='recipe-form-line'>
+						<label htmlFor='recipe-title-submission'>Title</label>
+						<submission id='recipe-title-submission' key='title' name='title' type='text' value={title}
+						size={42} autoComplete="off" onChange={this.handleChange}/>
+					</div>
 
-										< submission
-											id='recipe-title-submission'
-											key='title'
-											name='title'
-											type='text'
-											value={title}
-											size={42}
-											autoComplete="off"
-											onChange={this.handleChange}
-										/>
-										< /div>
-											< label
-												htmlFor='recipe-instructions-submission'
-												style={
-													{
-														marginTop: '5px'
-													}
-												}>
-												Instructions < /label>
+				<label htmlFor='recipe-instructions-submission' style={{marginTop: '5px'}}>Instructions</label>
 
-		< textarea
-			key='instructions'
-			id='recipe-instructions-submission'
-			type='Instructions'
-			name='instructions'
-			rows='8'
-			cols='50'
-			autoComplete='off'
-			value={instructions}
-			onChange={this.handleChange}
+		<textarea key='instructions' id='recipe-instructions-submission' type='Instructions' name='instructions'
+					 rows='8' cols='50' autoComplete='off' value={instructions} onChange={this.handleChange}
 		/> {submissions}
 
-		<button type="button" onClick={this.handleNewIngredient} className="buttons">+</button>
+				<button type="button" onClick={this.handleNewIngredient} className="buttons">+</button>
 
 			<div className='recipe-form-line'>
-			<label htmlFor='recipe-img-submission'> Image Url</label>
-			<submission id='recipe-img-submission' type='text' placeholder='' name='img' value={img} size={36}
-			autoComplete='off' onChange={this.handleChange}/>
+				<label htmlFor='recipe-img-submission'> Image Url</label>
+				<submission id='recipe-img-submission' type='text' placeholder='' name='img' value={img} size={36}
+				autoComplete='off' onChange={this.handleChange}/>
 			</div>
 
 				<button type="submit" className="buttons" style={{alignSelf: 'flex-end', marginRight: 0}}>SAVE</button>
 
 			</form>
-		< /div>
+		</div>
 	}
 }
 
