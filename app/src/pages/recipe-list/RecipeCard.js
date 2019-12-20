13251bf69c5919e9113cc0d5e755ebfa8857a56e
Floribella Ponce <fponce2@cnm.edu>
2019-12-19 21:09:35 -0700

    reduced card margin, removed body margin, and added medium size column

--- app/src/pages/recipe-list/RecipeCard.js
+++ app/src/pages/recipe-list/RecipeCard.js
@@ -8,13 +8,13 @@ export const RecipeCard = ({recipe}) => {
 
 //this gives form to the recipes in the list on DOM
 <Route render={ ({history}) => (
-		<Card className="mb-4 mr-5" key={recipe.recipeId} onClick={() => {history.push(`recipe-page.js/${recipe.recipeId}`)}}>
+		<Card className="mb-4 mr-3" key={recipe.recipeId} onClick={() => {history.push(`recipe-page.js/${recipe.recipeId}`)}}>
 		{/*<Card className="mb-4 mr-5">*/}
-			<Card.Body className="row my-3 mx-3">
-				<div className="col-3">
+			<Card.Body className="row">
+				<div className="col-md-3 d-none d-sm-block">
 					<Card.Img src={recipe.recipeImageUrl ? recipe.recipeImageUrl : logo} alt="placeholder"/>
 				</div>
-				<div className="col-9" data-categoryId={recipe.recipeCategoryId}>
+				<div className="col-md-9" data-categoryId={recipe.recipeCategoryId}>
 					<Card.Title> {recipe.recipeName}</Card.Title>
 					<Card.Subtitle>Cook Time: {recipe.recipeMinutes} mins. Rating: {recipe.recipeRating}</Card.Subtitle>
 					<Card.Text>Description: {recipe.recipeDescription}</Card.Text>
